61918a5067f7f1a3bc64bdbfd5a3ed2c0afdfadc
Justin <justinadams758@gmail.com>
2022-01-24 15:15:26 -0600

    added progress bars and logic for coloring

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BudgetsProvider } from "./context/BudgetContext"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BudgetsProvider>
+    <App />
+    </BudgetsProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
